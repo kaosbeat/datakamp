409990e69967bdd09ffef7246e5d855417e11ee7
devriesewouter89 <devriese.wouter@gmail.com>
2017-07-10 20:16:02 +0200

    Update MobileReader_RPi.py

--- MobileReader_RPi.py
+++ MobileReader_RPi.py
@@ -213,7 +213,7 @@ card_info = card.info('cardselect v0.1m')
 try:
     while 1:  
         card_id = listen(card, 0.1)
-	    listen_remove(card, 0.1, card_id)
+	listen_remove(card, 0.1, card_id)
 except KeyboardInterrupt:
         GPIO.cleanup()
 except:
