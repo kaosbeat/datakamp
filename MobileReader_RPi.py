5a1eb204191ad6747b602033802b2dedada1a7df
devriesewouter89 <devriese.wouter@gmail.com>
2017-07-10 20:06:09 +0200

    Update MobileReader_RPi.py

--- MobileReader_RPi.py
+++ MobileReader_RPi.py
@@ -210,10 +210,13 @@ card_info = card.info('cardselect v0.1m')
 
 
 # Main loop
-while 1:
-	card_id = listen(card, 0.1)
-	listen_remove(card, 0.1, card_id)
-    GPIO.cleanup()    
+try:
+        while 1:
+		card_id = listen(card, 0.1)
+		listen_remove(card, 0.1, card_id)
+except KeyboardInterrupt:
+	GPIO.cleanup()
+       
 
 
 #Read RFID
