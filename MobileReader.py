6de13122eb4c0be9f2a09adfd180abb33ef9eb0d
devriesewouter89 <devriese.wouter@gmail.com>
2017-07-11 18:19:07 +0200

    Update MobileReader.py

--- MobileReader.py
+++ MobileReader.py
@@ -85,14 +85,14 @@ def playAudio(userType):
 #         mixer.init()
         print "first play"
         dir = os.path.dirname(__file__)
-        print location
+        
         if "Basic" in userType: 
-            filename = os.path.join(dir, 'soundboard/',location,'basic.mp3')       
+            filename = os.path.join(dir, 'soundboard/Mobile/basic.mp3')       
         else: 
 		if "Premium VIP" in userType :
-            		filename = os.path.join(dir, 'soundboard/',location,'premium_vip.mp3')
+            		filename = os.path.join(dir, 'soundboard/Mobile/premium_vip.mp3')
 		else: 
-                	filename = os.path.join(dir, 'soundboard/',location,'vip.mp3')
+                	filename = os.path.join(dir, 'soundboard/Mobile/vip.mp3')
         print filename
         mixer.music.load(filename)
         mixer.music.play()
