3397334a1bf62c771d853647db1403c8f02bc4aa
Wouter Devriese <wdevries@LAPTOP-1501156.local>
2017-07-12 12:50:26 +0200

    modular config file

---
+++ config_RPi.py
@@ -0,0 +1,19 @@
+import socket
+#this file is the only file that is different on each reader device (mobile or fixed reader)
+print(socket.gethostname())
+
+settings = {
+"readerType": "mobile",
+"readerID": socket.gethostname()
+}
+# types of readerID:
+# Ingang
+# Premium
+# Kassa
+# Bar
+# Stempaal
+# Playfield
+# Gili
+# WC
+# Lichtpaal
+# Uitgang
