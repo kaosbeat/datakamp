16b376c33ac65e5fca0f1d2dead513704376cb7e
kaosbeat <kasper.jordaens@gmail.com>
2017-07-14 16:07:39 +0000

    lichtpaalcimmit

--- LichtpaalReader.py
+++ LichtpaalReader.py
@@ -6,10 +6,7 @@
 # RFID Read
 #
 
-
-
 import os,sys
-import time
 import json
 import time
 import rfidiot
@@ -26,89 +23,97 @@ print readerid
 
 #######################  DMX FUNCTIONS ################
 def DmxSent(state):
-	global dmxwrapper
-  	dmxwrapper.Stop()
+    global dmxwrapper
+    dmxwrapper.Stop()
 
 def SendDmx(dmxuniverse, dmxdata):
-	global dmxwrapper
-	dmxclient = dmxwrapper.Client()
-	dmxclient.SendDmx(dmxuniverse, dmxdata, DmxSent)
-	dmxwrapper.Run()
+    global dmxwrapper
+    dmxclient = dmxwrapper.Client()
+    dmxclient.SendDmx(dmxuniverse, dmxdata, DmxSent)
+    dmxwrapper.Run()
 
-def RedDMX(time):
-	global dmxuniverse
-	dmxdata = array.array('B', [7, 0, 0, 0 ,0])
-	SendDmx(dmxuniverse, dmxdata)
-	time.sleep(1)
-	dmxdata = array.array('B', [0, 0, 0, 0 ,0])
-	SendDmx(dmxuniverse, dmxdata)
-	time.sleep(0.7)
-	dmxdata = array.array('B', [7, 0, 0, 0 ,0])
-	SendDmx(dmxuniverse, dmxdata)
-	time.sleep(0.5)
-	dmxdata = array.array('B', [0, 0, 0, 0 ,0])
-	SendDmx(dmxuniverse, dmxdata)
-	time.sleep(0.3)
-	dmxdata = array.array('B', [7, 0, 0, 0 ,0])
-	SendDmx(dmxuniverse, dmxdata)
-	time.sleep(0.1)
-	dmxdata = array.array('B', [7, 0, 0, 128 ,0])
-	SendDmx(dmxuniverse, dmxdata)
-	time.sleep(0.1)
-	dmxdata = array.array('B', [0, 0, 0, 0 ,0])
-	SendDmx(dmxuniverse, dmxdata)
+def RedDMX(sleeptime):
+    global dmxuniverse
+    dmxdata = array.array('B', [7, 0, 0, 0 ,0])
+    SendDmx(dmxuniverse, dmxdata)
+    time.sleep(1)
+    dmxdata = array.array('B', [0, 0, 0, 0 ,0])
+    SendDmx(dmxuniverse, dmxdata)
+    time.sleep(0.7)
+    dmxdata = array.array('B', [7, 0, 0, 0 ,0])
+    SendDmx(dmxuniverse, dmxdata)
+    time.sleep(0.5)
+    dmxdata = array.array('B', [0, 0, 0, 0 ,0])
+    SendDmx(dmxuniverse, dmxdata)
+    time.sleep(0.3)
+    dmxdata = array.array('B', [7, 0, 0, 0 ,0])
+    SendDmx(dmxuniverse, dmxdata)
+    time.sleep(0.1)
+    dmxdata = array.array('B', [7, 0, 0, 128 ,0])
+    SendDmx(dmxuniverse, dmxdata)
+    time.sleep(0.1)
+    dmxdata = array.array('B', [0, 0, 0, 0 ,0])
+    SendDmx(dmxuniverse, dmxdata)
 
-def YellowDMX(time):
-	global dmxuniverse
-	dmxdata = array.array('B', [19, 0, 0, 0 ,0])
-	SendDmx(dmxuniverse, dmxdata)
-	
-def GreenDMX(time):
-	global dmxuniverse
-	dmxdata = array.array('B', [13, 0, 0, 0 ,0])
-	SendDmx(dmxuniverse, dmxdata)
+def YellowDMX(sleeptime):
+    global dmxuniverse
+    dmxdata = array.array('B', [19, 0, 0, 0 ,0])
+    SendDmx(dmxuniverse, dmxdata)
+    time.sleep(1)
+    dmxdata = array.array('B', [0, 0, 0, 0 ,0])
+    SendDmx(dmxuniverse, dmxdata)
+    
+
+def GreenDMX(sleeptime):
+    global dmxuniverse
+    dmxdata = array.array('B', [13, 0, 0, 0 ,0])
+    SendDmx(dmxuniverse, dmxdata)
+    time.sleep(1)
+    dmxdata = array.array('B', [0, 0, 0, 0 ,0])
+    SendDmx(dmxuniverse, dmxdata)
 
 # Card reader Functions
 def open_reader():
-	""" Attempts to open the card reader """
-	try:
-		card = rfidiot.card
-		return card
-	except:
-		print "Couldn't open reader!"
-		sys.exit()
-		return None
+    """ Attempts to open the card reader """
+    try:
+        card = rfidiot.card
+        return card
+    except:
+        print "Couldn't open reader!"
+        sys.exit()
+        return None
 
 def listen(card, interval):
-	""" Listens for a card to be placed on the reader """
-	while 1:
-	if card.select():
-	    # print readerid
-        data = getVistorActions(card.uid)
-        if (data['visitortype'] == "Premium VIP"):
-        	# print data
-            # INSERT DMX CODE HERE KASPER
-            RedDMX(100)
-        elif (data['visitortype'] == "Premium"):
-        	YellowDMX(100)
-        elif (data['visitortype'] == "Basic"):
-        	GreenDMX(100)   
-        #print 'Waiting: Card Placement'
-		time.sleep(interval)
-		return card.uid
+    """ Listens for a card to be placed on the reader """
+    while 1:
+        if card.select():
+            # print readerid
+            logAction(readerid, card.uid, "ACT")
+            data = getVistorActions(card.uid)
+            if (data['visitortype'] == "Premium VIP"):
+                # print data
+                # INSERT DMX CODE HERE KASPER
+                RedDMX(100)
+            elif (data['visitortype'] == "Premium"):
+                YellowDMX(100)
+            elif (data['visitortype'] == "Basic"):
+                GreenDMX(100)   
+            #print 'Waiting: Card Placement'
+            time.sleep(interval)
+            return card.uid
 
 def listen_remove(card, interval, card_id):
-	""" Listens for a card to be placed on the reader """
-	# Screen.wrapper(datascreen)
-	while 1:
-		screensaverstate = 1
-		if not card.select():
-			# data = json.dumps({"card_info":
-			# 	[{"card_id": card_id}, {"timedate": get_time()}, {"action": "Removed"}]})
-			# print(data)
-			break
-		#print "Waiting: Card Removal"
-		time.sleep(interval)
+    """ Listens for a card to be placed on the reader """
+    # Screen.wrapper(datascreen)
+    while 1:
+        screensaverstate = 1
+        if not card.select():
+            # data = json.dumps({"card_info":
+            #   [{"card_id": card_id}, {"timedate": get_time()}, {"action": "Removed"}]})
+            # print(data)
+            break
+        #print "Waiting: Card Removal"
+        time.sleep(interval)
     return None
        
 
@@ -122,8 +127,8 @@ card_info = card.info('cardselect v0.1m')
 try:
     while 1:  
         card_id = listen(card, 0.1)
-	listen_remove(card, 0.1, card_id)
+    listen_remove(card, 0.1, card_id)
 except KeyboardInterrupt:
-	print "keyboard interrupt!"
+    print "keyboard interrupt!"
 
 
